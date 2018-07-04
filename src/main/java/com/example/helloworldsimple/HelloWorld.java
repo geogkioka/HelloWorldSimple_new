75f74d3a7fa470b72c722da07e9047f57a18e3ce
Georgia Gkioka <georgiagkioka@Georgias-MacBook-Pro.local>
2018-07-04 20:48:34 +0300

    first commit

---
+++ src/main/java/com/example/helloworldsimple/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworldsimple;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("It's the first test");
+    }
+}
+
+
---
+++ src/test/java/com/example/helloworldsimple/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example.helloworldsimple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void main() {
+    }
+}
